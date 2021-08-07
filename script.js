39ef4bb4f48d1483f8180782e4997560383d1a57
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-08-06 21:35:08 -0300

    adicionando funcionalidades com js

---
+++ script.js
@@ -0,0 +1,57 @@
+const controllers = document.querySelector('#controllers');
+const textCss = document.querySelector('.css');
+const btn = document.querySelector('.btn');
+
+controllers.addEventListener('change', handleChange)
+
+const handleStyle = {
+   element: btn,
+   backgroundColor(value) {
+      this.element.style.backgroundColor = value
+   },
+
+   height(value) {
+      this.element.style.height = value + 'px';
+   },
+
+   width(value) {
+      this.element.style.width = value + 'px';
+   },
+
+   color(value) {
+      this.element.style.color = value;
+   },
+
+   border(value) {
+      this.element.style.border = value;
+   },
+
+   borderRadius(value) {
+      this.element.style.borderRadius = value + 'px';
+   },
+
+   fontFamily(value) {
+      this.element.style.fontFamily = value;
+   },
+
+   fontSize(value) {
+      this.element.style.fontSize = value + 'rem';
+   },
+
+   texto(value) {
+      this.element.innerText = value;
+   },
+}
+
+function handleChange(event) {
+   const name = event.target.name;
+   const value = event.target.value;
+   handleStyle[name](value);
+   showCss();
+}
+
+function showCss() {
+   textCss.innerHTML = '<span>' + btn.style.cssText
+      .split('; ')
+      .join(';</span><span>')
+}
